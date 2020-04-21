158abdc184cbddfc72e36f967cd03508d25b6e42
Udi h Bauman <dibaunaumh@gmail.com>
2020-04-21 06:55:07 -0700

    docker deployment

---
+++ bldg_server/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Docker entrypoint script.
+
+# Wait until Postgres is ready
+while ! pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER
+do
+    echo "${date} - waiting for database to start"
+    sleep 2
+done
+
+
+./prod/rel/bldg_server/bin/bldg_server eval BldgServer.Release.migrate
+
+./prod/rel/bldg_server/bin/bldg_server start
