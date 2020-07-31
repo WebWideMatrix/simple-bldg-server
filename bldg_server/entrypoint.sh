a3ae8660633bc9517feab6d508a59c20a0f380fd
Udi h Bauman <dibaunaumh@gmail.com>
2020-07-30 17:43:17 -0700

    use the new db-port also in the entrypoint

--- bldg_server/entrypoint.sh
+++ bldg_server/entrypoint.sh
@@ -2,7 +2,7 @@
 # Docker entrypoint script.
 
 # Wait until Postgres is ready
-while ! pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER
+while ! pg_isready -q -h $DB_HOST -p $DB_PORT -U $DB_USER
 do
     echo "${date} - waiting for database to start"
     sleep 2
