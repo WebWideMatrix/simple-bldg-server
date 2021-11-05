6153754f36d1682fb0decedba36d8a3df6c93004
Udi h Bauman <dibaunaumh@gmail.com>
2021-11-04 18:56:52 -0700

    fix a bug in extract_coords - didn't work with negative coords :(

--- bldg_server/lib/bldg_server/buildings.ex
+++ bldg_server/lib/bldg_server/buildings.ex
@@ -130,12 +130,12 @@ defmodule BldgServer.Buildings do
   # UTILS
 
   def extract_coords(addr) do
-    # get the last part of the address: "g/b(17,24)/l0/b(11,6)" -> "b(11,6)"
-    coords_token = addr
+    # get the coords from the last part of the address: "g/b(17,24)/l0/b(-11,6)" -> ["-11","6]
+    [x_s, y_s] = addr
     |> String.split(address_delimiter)
     |> List.last()
-    # get the coordinates: "b(11,6)" -> (11,6)
-    [[x_s], [y_s]] = Regex.scan(~r{\d+}, coords_token)
+    |> String.slice(2..-2)
+    |> String.split(",")
     {{x, ""}, {y, ""}} = {Integer.parse(x_s), Integer.parse(y_s)}
     {x, y}
   end
