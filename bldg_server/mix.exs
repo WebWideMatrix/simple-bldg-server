b9c9b6f28d2154b1251247c94cf4ea6a6bfcba7a
Udi h Bauman <dibaunaumh@gmail.com>
2022-09-01 12:29:36 -0700

    temporarily revert the cloack changes as they are faling in prod, & not yet in use

--- bldg_server/mix.exs
+++ bldg_server/mix.exs
@@ -4,7 +4,7 @@ defmodule BldgServer.MixProject do
   def project do
     [
       app: :bldg_server,
-      version: "0.5.0",
+      version: "0.5.4",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
@@ -46,8 +46,7 @@ defmodule BldgServer.MixProject do
       {:json, "~> 1.4"},
       {:finch, "~> 0.7"},
       {:bamboo, "~> 1.7.1"},
-      {:bamboo_smtp, "~> 3.1.3"},
-      {:cloak_ecto, "~> 1.2.0"}
+      {:bamboo_smtp, "~> 3.1.3"}
     ]
   end
 
